182a5ccf30f47e22730f8ca355bf7541c4928e14
shakmarev <ses590@g.harvard.edu>
2021-05-11 16:10:13 +0300

    Fixing readthedocs issues.

--- source/conf.py
+++ source/conf.py
@@ -29,9 +29,7 @@ author = 'Sergey Shakmarev'
 # ones.
 extensions = [
     'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.coverage',
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,8 +53,3 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-import mock
-
-MOCK_MODULES = ['awscli', 'pandas', 'xlrd', 'cython', 'pytest', 'pytest-cov', 'colorama', 'luigi', 'requests', 'requests.exceptions', 'aiohttp', 'yfinance', 'rpy2', 'rpy2.robjects', 'rpy2.robjects.packages', 'flask', 'gitpython', 'canvasapi']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()

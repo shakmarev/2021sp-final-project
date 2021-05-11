dfd70e1b2d2f67ac0506ccf42fab84235ea963a8
shakmarev <ses590@g.harvard.edu>
2021-05-11 15:35:41 +0300

    Fixing readthedocs issues.

--- source/conf.py
+++ source/conf.py
@@ -54,3 +54,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import mock
+
+MOCK_MODULES = ['awscli', 'pandas', 'xlrd', 'cython', 'pytest', 'pytest-cov', 'colorama', 'luigi', 'requests', 'aiohttp', 'yfinance', 'rpy2', 'flask', 'gitpython', 'canvasapi']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
